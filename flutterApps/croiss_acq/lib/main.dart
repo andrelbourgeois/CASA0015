69165fcf453e69971356b1ff00ee88c8abcc2464
André Bourgeois <>
2022-03-30 21:34:35 +0100

    added more pages, improved navigation
    added more pages, set up the drawer menu and back button, realized that implementing the drawer on each page would be very repetitive - decided to turn the drawer into a class and call it on each page (fucking class idea if i do say so myself)
    Once i had multi-page navigation implemented, i realized that the back button only popped a single screen off the stack, so the most recent edit included here was to change the back buttons so that they pop screens until the home screen is reached.

--- flutterApps/croiss_acq/lib/main.dart
+++ flutterApps/croiss_acq/lib/main.dart
@@ -1,21 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({Key? key}) : super(key: key);
-  static const appTitle = 'CROISSACQ';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: appTitle,
-      theme: ThemeData(
+      title: 'CROISSACQ',
+      theme: ThemeData(),
+      home: MyHomePage(title: 'MyHomePage'),
+    );
+  }
+}
 
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            // TODO add profile ICON and link to profile page
+            child: Text('I want a profile ICON here'),
+          ),
+          ListTile(
+            title: const Text('Map View'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MapView(
+                          title: 'MapView',
+                        )),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Favourites'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const Favourites(
+                          title: 'Favourites',
+                        )),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Pain au Chocolat?'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const PainAuChocolat(
+                          title: 'PainAuChocolat',
+                        )),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Settings'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const Settings(
+                          title: 'Settings',
+                        )),
+              );
+            },
+          ),
+        ],
       ),
-      home: MyHomePage(title: appTitle),
     );
   }
 }
@@ -29,7 +100,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xffffffff),
+        backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
         centerTitle: true,
         title: const Text(
@@ -37,71 +108,9 @@ class MyHomePage extends StatelessWidget {
           style: TextStyle(color: Color(0xff000000)),
         ),
       ),
+      endDrawer: MyDrawer(),
       body: const Center(
-        child: Text('Coming Soon'),
-      ),
-      endDrawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.white,
-              ),
-              // TODO add profile ICON and link to profile page
-              child: Text('I want a profile ICON here'),
-            ),
-            ListTile(
-              title: const Text('Map View'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MapView(title: 'MapView',)),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('List View'),
-              onTap: () {
-                // TODO Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Favourites'),
-              onTap: () {
-                // TODO Updated the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Pain au Chocolat?'),
-              onTap: () {
-                // TODO Updated the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Settings'),
-              onTap: () {
-                // TODO Updated the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+        child: Text('Home Page Coming Soon'),
       ),
     );
   }
@@ -117,82 +126,101 @@ class MapView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).popUntil((route) => route.isFirst),
         ),
-        backgroundColor: Color(0xffffffff),
+        backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
         centerTitle: true,
         title: const Text(
           'CROISSACQ',
-          style: TextStyle(color: Color(0xff000000)),
+          style: TextStyle(color: Colors.black),
         ),
       ),
+      endDrawer: MyDrawer(),
       body: const Center(
-        child: Text('Coming Soon #2'),
+        child: Text('Map View Coming Soon'),
       ),
-      endDrawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.white,
-              ),
-              // TODO add profile ICON and link to profile page
-              child: Text('I want a profile ICON here'),
-            ),
-            ListTile(
-              title: const Text('Map View'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MapView(title: 'MapView',)),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('List View'),
-              onTap: () {
-                // TODO Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Favourites'),
-              onTap: () {
-                // TODO Updated the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Pain au Chocolat?'),
-              onTap: () {
-                // TODO Updated the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Settings'),
-              onTap: () {
-                // TODO Updated the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
+    );
+  }
+}
+
+class Favourites extends StatelessWidget {
+  const Favourites({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).popUntil((route) => route.isFirst),
         ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        centerTitle: true,
+        title: const Text(
+          'CROISSACQ',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      endDrawer: MyDrawer(),
+      body: const Center(
+        child: Text('Favourites Coming Soon'),
+      ),
+    );
+  }
+}
+
+class PainAuChocolat extends StatelessWidget {
+  const PainAuChocolat({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).popUntil((route) => route.isFirst),
+        ),
+        backgroundColor: Color(0xffffffff),
+        iconTheme: IconThemeData(color: Colors.black),
+        centerTitle: true,
+        title: const Text('CROISSACQ', style: TextStyle(color: Colors.black)),
+      ),
+      endDrawer: MyDrawer(),
+      body: const Center(
+        child: Text('Pain au Chocolat Coming Soon'),
+      ),
+    );
+  }
+}
+
+class Settings extends StatelessWidget {
+  const Settings({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).popUntil((route) => route.isFirst),
+        ),
+        backgroundColor: Color(0xffffffff),
+        iconTheme: IconThemeData(color: Colors.black),
+        centerTitle: true,
+        title: const Text('CROISSACQ', style: TextStyle(color: Colors.black)),
+      ),
+      endDrawer: MyDrawer(),
+      body: const Center(
+        child: Text('Settings Coming Soon'),
       ),
     );
   }
